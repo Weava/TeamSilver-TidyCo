faf0f7b21bafe104adadd3021a1b357d655aa8f4
Aaron Weaver <waaronl@okstate.edu>
2015-10-08 18:17:39 -0500

    Initial Commit

---
+++ TidyCoTests/TidyCoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TidyCoTests.swift
+//  TidyCoTests
+//
+//  Created by Aaron Weaver on 10/8/15.
+//  Copyright © 2015 Team Silver. All rights reserved.
+//
+
+import XCTest
+@testable import TidyCo
+
+class TidyCoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
