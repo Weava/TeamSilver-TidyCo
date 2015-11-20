6da4e534b85909a2ecda28c54598785e9c58da1f
Austin Keeton <akeeton94@gmail.com>
2015-11-19 19:31:09 -0600

    Can add new timers.

--- TidyCo/RoomsListTableViewController.swift
+++ TidyCo/RoomsListTableViewController.swift
@@ -111,6 +111,7 @@ class RoomsListTableViewController: UITableViewController {
         return headerHeight
     }
     
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
